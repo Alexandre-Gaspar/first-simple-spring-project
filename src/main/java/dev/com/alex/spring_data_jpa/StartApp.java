93f923012d6ec426edfe83acc32f5173030164ce
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-06-16 00:37:27 +0100

    feat(refactor): create a insertUser

--- src/main/java/dev/com/alex/spring_data_jpa/StartApp.java
+++ src/main/java/dev/com/alex/spring_data_jpa/StartApp.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import dev.com.alex.spring_data_jpa.model.User;
 import dev.com.alex.spring_data_jpa.repository.UserRepository;
 
+import java.util.List;
 import java.util.Scanner;
 
 @Component
@@ -20,34 +21,42 @@ public class StartApp implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+        insertUser();
+
+        System.out.println("\n");
+        List<User> users = userRepository.filterByName("Alexandre Gaspar");
+        users.forEach(System.out::println);
+    }
+
+    // This method allows insert users into table "tb_user" by command line
+    private void insertUser() {
         while (true) {
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-
-            System.out.print("Username: ");
-            String username = sc.nextLine();
-
-            System.out.print("Password: ");
-            String password = sc.nextLine();
-
-            User user = new User();
-            user.setName(name);
-            user.setUsername(username);
-            user.setPassword(password);
-            userRepository.save(user);
-
             System.out.print("Add more ? [y/n]: ");
-            String decision = String.valueOf(sc.nextLine().charAt(0));
+            String decision = String.valueOf(sc.nextLine().charAt(0)); // Take the first character of the answer
+
             if (decision.equalsIgnoreCase("y")) {
-                continue;
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Username: ");
+                String username = sc.nextLine();
+
+                System.out.print("Password: ");
+                String password = sc.nextLine();
+
+                User user = new User();
+                user.setName(name);
+                user.setUsername(username);
+                user.setPassword(password);
+                userRepository.save(user);
             } else {
                 break;
             }
         }
 
-
-        for (User u: userRepository.findAll())
-            System.out.println(u);
+        // Return all users in the table
+//        for (User u: userRepository.findAll())
+//            System.out.println(u);
     }
-    
+
 }
